99a400dfa3f0ccd82857fc0bc595217d662f1eb7
Gaviria15 <samuelstiven2005@gmail.com>
2023-11-02 23:34:54 -0500

    Modificaciones

--- js/script.js
+++ js/script.js
@@ -103,3 +103,15 @@ function vaciarCarrito(){
     }
     return false
 }
+
+
+const iconoUsuarios = document.getElementById('icono-usuarios');
+const subMenuUsuarios = document.getElementById('sub-menu-usuarios');
+
+iconoUsuarios.addEventListener('click', () => {
+    if (subMenuUsuarios.style.display === 'block') {
+        subMenuUsuarios.style.display = 'none';
+    } else {
+        subMenuUsuarios.style.display = 'block';
+    }
+});
